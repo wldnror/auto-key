06de79cb12b353b6ea8a60e644b44813b944ddbe
wldnror <72863449+wldnror@users.noreply.github.com>
2024-01-30 22:56:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-# 예: 'a' 키를 누르는 HID 리포트
+import os
+import time
+
+# 'a' 키를 누르는 HID 리포트 전송
 os.system('echo -ne "\\x00\\x00\\x04\\x00\\x00\\x00\\x00\\x00" > /dev/hidg0')  # 'a' 키 누름
 time.sleep(0.1)
 os.system('echo -ne "\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00" > /dev/hidg0')  # 모든 키 뗌
