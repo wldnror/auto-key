d0ed13161c6cd83c4aacd911a648f8d7dd0f6b79
wldnror <72863449+wldnror@users.noreply.github.com>
2024-01-31 12:17:35 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import time
 
 # 'a' 키를 누르는 HID 리포트 전송
 time.sleep(3)
-os.system('echo -ne "\\x00\\x00\\x04\\x00\\x00\\x00\\x00\\x00" > /dev/hidg0')  # 'a' 키 누름
-time.sleep(11.1)
-os.system('echo -ne "\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00" > /dev/hidg0')  # 모든 키 뗌
+os.system('sudo bash -c "echo -ne \\"\\x00\\x00\\x04\\x00\\x00\\x00\\x00\\x00\\" > /dev/hidg0"')  # 'a' 키 누름
+time.sleep(0.1)
+os.system('sudo bash -c "echo -ne \\"\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\" > /dev/hidg0"')  # 모든 키 뗌
