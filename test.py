9adb5cce212b91c44c222a3a0a345349cadba583
wldnror <72863449+wldnror@users.noreply.github.com>
2024-01-31 19:18:05 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-
-# 'a' 키를 누르는 HID 리포트 전송
-time.sleep(3)
-os.system('sudo bash -c "echo -ne \\"\\x00\\x00\\x04\\x00\\x00\\x00\\x00\\x00\\" > /dev/hidg0"')  # 'a' 키 누름
-time.sleep(0.1)
-os.system('sudo bash -c "echo -ne \\"\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\" > /dev/hidg0"')  # 모든 키 뗌
