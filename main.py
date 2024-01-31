658d28edf47e304dd7117033799d56c31b6c6e2e
wldnror <72863449+wldnror@users.noreply.github.com>
2024-01-31 12:56:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ def emulate_keyboard():
     # Shift + '=' 키 입력
     # os.system('echo -ne "\\x02\\x38" > /dev/hidg0')  # Shift 키 누름
     time.sleep(3)
-    os.system('sudo bash -c "echo -ne \\"\\x00\\x00\\x2E\\x00\\x00\\x00\\x00\\x00\\" > /dev/hidg0"')  # '=' 키 누름
+    os.system('sudo bash -c "echo -ne \\"\\x02\\x00\\x2E\\x00\\x00\\x00\\x00\\x00\\" > /dev/hidg0"')  # 시프트 + '=' 키 누름
     time.sleep(0.1)
     os.system('sudo bash -c "echo -ne \\"\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\" > /dev/hidg0"')  # 모든 키 뗌
-
 # 버튼 눌림 이벤트
 button.when_pressed = emulate_keyboard
 
